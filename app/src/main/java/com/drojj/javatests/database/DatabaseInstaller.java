a34c5f3f446edbfb8440bb6e9bc66333015b29fa
drojjipivnie <drojjipivnie@gmail.com>
2017-09-02 15:36:25 +0300

    Added Databaseinstaller

---
+++ app/src/main/java/com/drojj/javatests/database/DatabaseInstaller.java
@@ -0,0 +1,105 @@
+package com.drojj.javatests.database;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.drojj.javatests.app.App;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.inject.Inject;
+
+import timber.log.Timber;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class DatabaseInstaller {
+
+    @Inject
+    Context context;
+
+    private static final String APP_PREFERENCES = "app_settings";
+
+    private static final String APP_PREFERENCES_DB_SETTING = "database_version";
+
+    private final static String mDatabaseName = "Database.db";
+
+    private final static String mAssetsDatabasePath = "database/" + mDatabaseName;
+
+    private final String mLocalDatabasePath;
+
+    public DatabaseInstaller() {
+        App.getAppComponent().inject(this);
+        if (android.os.Build.VERSION.SDK_INT >= 17) {
+            mLocalDatabasePath = context.getApplicationInfo().dataDir + "/databases";
+        } else {
+            mLocalDatabasePath = context.getFilesDir().getPath() + context.getPackageName() + "/databases";
+        }
+    }
+
+    public void install() throws IOException {
+        if (!isDatabaseInstalled()) {
+            Timber.d("Database not exists. Need to be installed");
+            copyDatabase();
+        } else {
+            Timber.d("Database exists. Checking database update state. Database version must be version: %s", AppDatabase.VERSION);
+            int databaseVersion = getDatabaseVersion();
+            Timber.d("Current version is %s", databaseVersion);
+            if (databaseVersion == AppDatabase.VERSION) {
+                Timber.d("Database is at the last version. No need to update.");
+            } else {
+                Timber.d("Database version invalid. Installing new database.");
+                copyDatabase();
+            }
+        }
+    }
+
+    private int getDatabaseVersion() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE);
+        return sharedPreferences.getInt(APP_PREFERENCES_DB_SETTING, 0);
+    }
+
+    private boolean isDatabaseInstalled() {
+        File localDatabase = new File(mLocalDatabasePath + "/" + mDatabaseName);
+        return localDatabase.exists();
+    }
+
+    private void copyDatabase() throws IOException {
+        InputStream input = null;
+        OutputStream output = null;
+
+        try {
+            input = context.getAssets().open(mAssetsDatabasePath);
+
+            File databaseDirectory = new File(mLocalDatabasePath);
+            if (!databaseDirectory.exists()) {
+                databaseDirectory.mkdir();
+            }
+
+            output = new FileOutputStream(mLocalDatabasePath + "/" + mDatabaseName);
+
+            byte[] mBuffer = new byte[1024];
+            int mLength;
+            while ((mLength = input.read(mBuffer)) > 0) {
+                output.write(mBuffer, 0, mLength);
+            }
+
+            SharedPreferences sharedPreferences = context.getSharedPreferences(APP_PREFERENCES, MODE_PRIVATE);
+            sharedPreferences.edit().putInt(APP_PREFERENCES_DB_SETTING, AppDatabase.VERSION).apply();
+        } finally {
+
+            if (input != null) {
+                input.close();
+            }
+            if (output != null) {
+                output.flush();
+                output.close();
+            }
+            Timber.d("Database installed successfully with version: %s", AppDatabase.VERSION);
+        }
+    }
+}
--- app/src/main/java/com/drojj/javatests/ui/activity/SplashActivity.java
+++ app/src/main/java/com/drojj/javatests/ui/activity/SplashActivity.java
@@ -1,18 +1,23 @@
 package com.drojj.javatests.ui.activity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.arellomobile.mvp.MvpActivity;
-import com.drojj.javatests.presentation.view.SplashView;
-import com.drojj.javatests.presentation.presenter.SplashPresenter;
-
-import com.drojj.javatests.R;
-
 import com.arellomobile.mvp.presenter.InjectPresenter;
+import com.drojj.javatests.R;
+import com.drojj.javatests.database.DatabaseInstaller;
+import com.drojj.javatests.presentation.presenter.SplashPresenter;
+import com.drojj.javatests.presentation.view.SplashView;
 import com.drojj.javatests.ui.activity.auth.SignInActivity;
 import com.google.firebase.auth.FirebaseAuth;
 
+import io.reactivex.Observable;
+import io.reactivex.disposables.Disposable;
+
 public class SplashActivity extends MvpActivity implements SplashView {
+
+    Disposable disposable;
 
     @InjectPresenter
     SplashPresenter mSplashPresenter;
@@ -21,12 +26,30 @@ public class SplashActivity extends MvpActivity implements SplashView {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        mSplashPresenter.checkAuthorized(FirebaseAuth.getInstance().getCurrentUser());
+
+        Observable<Void> observable = Observable.create(e -> {
+            new DatabaseInstaller().install();
+            e.onComplete();
+        });
+
+        disposable = observable.subscribe((v) -> {
+        }, (throwable) -> {
+            Toast.makeText(this, getString(R.string.database_installer_error, throwable.getLocalizedMessage()), Toast.LENGTH_SHORT).show();
+        }, () -> {
+            mSplashPresenter.checkAuthorized(FirebaseAuth.getInstance().getCurrentUser());
+        });
+
     }
 
     @Override
     public void setAuthorized(boolean isAuthorized) {
         SplashActivity.this.startActivity(isAuthorized ? HomeActivity.getIntent(this) : SignInActivity.getIntent(this));
         SplashActivity.this.finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disposable.dispose();
     }
 }
